0e15124b6fc1e47b575423c59ffabc45df25abf1
Johnathan Gilday <me@johnathangilday.com>
2019-11-16 15:09:31 -0500

    :arrow_up: Upgrade to Gradle 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("application")
     id("com.github.sherter.google-java-format") version "0.8"
     id("nebula.ospackage") version "6.1.1" apply false
-    id("com.google.cloud.tools.jib") version "1.1.2"
+    id("com.google.cloud.tools.jib") version "1.8.0"
 }
 
 apply(plugin = "nebula.rpm")
@@ -35,43 +35,44 @@ val junitVersion = "5.4.0"
 dependencies {
 
     // To avoid compiler warnings about @API annotations in JUnit code.
-    compile("org.apiguardian:apiguardian-api:1.0.0")
+    implementation("org.apiguardian:apiguardian-api:1.0.0")
 
     // jackson
-    compile("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-    compile("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    compile("com.fasterxml.jackson.module:jackson-module-parameter-names:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:$jacksonVersion")
     // jersey
-    compile("org.glassfish.jersey.core:jersey-server:$jerseyVersion")
-    compile("org.glassfish.jersey.inject:jersey-hk2:$jerseyVersion")
-    compile("org.glassfish.jersey.containers:jersey-container-jetty-http:$jerseyVersion")
-    compile("org.glassfish.jersey.media:jersey-media-json-jackson:$jerseyVersion") {
+    implementation("org.glassfish.jersey.core:jersey-server:$jerseyVersion")
+    implementation("org.glassfish.jersey.inject:jersey-hk2:$jerseyVersion")
+    implementation("org.glassfish.jersey.containers:jersey-container-jetty-http:$jerseyVersion")
+    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:$jerseyVersion") {
         exclude("com.fasterxml.jackson.core")
     }
     // jetty
-    compile("org.eclipse.jetty:jetty-server:$jettyVersion")
+    runtimeOnly("org.eclipse.jetty:jetty-server:$jettyVersion")
     // slf4j
-    compile("org.slf4j:slf4j-api:1.7.22")
-    compile("ch.qos.logback:logback-classic:1.1.3")
+    implementation("org.slf4j:slf4j-api:1.7.22")
+    runtimeOnly("ch.qos.logback:logback-classic:1.1.3")
     // typesafe config
-    compile("com.typesafe:config:1.3.1")
+    implementation("com.typesafe:config:1.3.1")
 
     // jaxb (no longer in JDK)
     implementation("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
 
     // test
-    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    testCompile("org.assertj:assertj-core:3.6.1")
-    testCompile("org.mockito:mockito-all:1.10.19")
-    testCompile("org.glassfish.jersey.test-framework.providers:jersey-test-framework-provider-jdk-http:$jerseyVersion") {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("org.assertj:assertj-core:3.6.1")
+    testImplementation("org.mockito:mockito-all:1.10.19")
+    testImplementation("org.glassfish.jersey.test-framework.providers:jersey-test-framework-provider-jdk-http:$jerseyVersion") {
         exclude("javax.servlet")
         exclude("junit")
     }
-    testCompile("org.glassfish.jersey.containers:jersey-container-jdk-http:$jerseyVersion")
+    testImplementation("org.glassfish.jersey.containers:jersey-container-jdk-http:$jerseyVersion")
 }
 
 tasks.compileJava {
+    // include parameter name information in the class files for Jackson to use at runtime
     options.compilerArgs = listOf("-parameters")
 }
 
@@ -85,7 +86,7 @@ val rpm by tasks.registering(Rpm::class) {
     dependsOn(distTar)
 
     packageName = project.name
-    version = project.version.toString()
+    archiveVersion.set(project.version.toString())
     release = "1.e7"
     os = org.redline_rpm.header.Os.LINUX
 
